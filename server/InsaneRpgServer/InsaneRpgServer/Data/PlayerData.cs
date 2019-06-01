25781a2d2d4ea16d2e74e87c35cc4ec270162516
Chris <ckim@gmail.com>
2019-06-01 12:22:01 -0400

    Initial server commit

---
+++ server/InsaneRpgServer/InsaneRpgServer/Data/PlayerData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InsaneRpgServer.Data
+{
+    public sealed class PlayerData
+    {
+        public double coordinateX { get; set; }
+        public double coordinateY;
+    }
+}
