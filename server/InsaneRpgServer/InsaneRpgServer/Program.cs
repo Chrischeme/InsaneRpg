26a508f7510b9279be85dd6b116992a6eac8f8c1
Chris <ckim@gmail.com>
2019-06-02 22:05:20 -0400

    Changed server code

--- server/InsaneRpgServer/InsaneRpgServer/Program.cs
+++ server/InsaneRpgServer/InsaneRpgServer/Program.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+﻿using SimpleTCP;
+using System;
 
-namespace InsaneRpgServer
+namespace ServerTest
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+            var server = new SimpleTcpServer().Start(8000);
+            server.DataReceived += (sender, msg) => {
+                Console.WriteLine($"Message was: {msg.MessageString}");
+                msg.Reply("Content-Type: text/plain\n\nHello from my web server!");
+            };
+            while (true)
+            {
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            }
+        }
     }
 }
